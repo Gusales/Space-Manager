b49012711b04ad96c8e64238a5fb7ee17a9eb78b
Gustavo Sales <85498221+Gusales@users.noreply.github.com>
2022-08-17 13:20:46 -0300

    Captcha
    Criação de um sistema captcha para evitar fraudes no sistema

---
+++ assets/js/login.js
@@ -0,0 +1,50 @@
+let iptLog = document.getElementById("rm");
+let iptPass = document.getElementById("senha");
+let iptCap = document.getElementById("captchaI");
+var captchaV = "";
+
+// CAPTCHA SYSTEM!!
+var allV = [
+    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "0","1","2","3","4","5","6","7","8","9",
+  ];
+function loadCap() {
+  var cVAl1 = allV[Math.floor(Math.random() * allV.length)];
+  var cVAl2 = allV[Math.floor(Math.random() * allV.length)];
+  var cVAl3 = allV[Math.floor(Math.random() * allV.length)];
+  var cVAl4 = allV[Math.floor(Math.random() * allV.length)];
+  var cVAl5 = allV[Math.floor(Math.random() * allV.length)];
+
+  captchaV = cVAl1 + cVAl2 + cVAl3 + cVAl4 + cVAl5;
+  console.log(captchaV);
+
+  document.getElementById("captchaV").innerHTML = captchaV;
+}
+
+function redirecionar() {
+  if (iptCap.value == "") {
+    alert("Captcha Não preenchido!");
+    captchaV = "";
+    loadCap();
+  } else if (iptCap.value != captchaV) {
+    alert("Captcha Errado!");
+    iptCap.value = "";
+    captchaV = "";
+    loadCap();
+  } else {
+    if (iptLog.value == "0000") {
+      if (iptPass.value == "1234") {
+        location.href = "lib/admin/home.html";
+      } else {
+        alert("Sua senha está errada Admin!");
+      }
+    } else if (iptLog.value == "" && iptPass.value == "") {
+      alert("Por favor, preencha os campos");
+    } else {
+      location.href = "assets/html/calendar.html";
+    }
+  }
+}
+function redirectLogin() {
+  location.href = "../index.html";
+}

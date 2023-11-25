f538766e04871fb2fd66e5d8d1d8109d71a0e8c0
Gustavo Sales <85498221+Gusales@users.noreply.github.com>
2023-11-25 13:23:50 -0300

    refactor: update seeds
    Created code for create database automatically and add three users

--- api/src/seed.js
+++ api/src/seed.js
@@ -1,26 +1,70 @@
 import 'dotenv/config'
+import mysql from 'mysql2/promise.js'
 import { hash } from 'bcrypt'
 import { sequelize, User } from './lib/sequelize.js'
 
-const userAdmin = {
-  name: 'Root',
-  email: 'root@email.com',
-  password: await hash('123456', 6),
+const users = [{
+  name: 'root',
+  email: 'gussales14@gmail.com',
+  password: '123456',
   rm: 99999,
+  telephone: '(11) 99999-9999',
   actype: 'ADMIN',
-}
+},
+{
+  name: 'professor 1',
+  email: 'gersons.br13@gmail.com',
+  password: '123456',
+  rm: 99998,
+  actype: 'PROF',
+},
+{
+  name: 'coordenador 1',
+  email: 'dev.gussales@gmail.com',
+  password: '123456',
+  rm: 99997,
+  actype: 'COORD',
+}]
 
-async function loadUsers() {
-  await sequelize.sync()
-
+async function seed(){
   try {
-    const user = await User.create(userAdmin)
+    const mysql_connection = await mysql.createConnection({
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD
+    })
 
-    console.log('Sucesso! usuário Administrador criado')
-    console.log(user)
+    await mysql_connection.query(`DROP DATABASE IF EXISTS ${process.env.DATABASE}`)
+    await mysql_connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DATABASE}`)
+    
+    await sequelize.sync()
+
+    users.forEach(async (user) => {
+      let userToCreate = await User.findOne({
+        where: {
+          rm: user.rm
+        }
+      })
+      
+      if (!userToCreate) {
+        await User.create({
+          name: user.name,
+          email: user.email,
+          rm: user.rm,
+          password: await hash(user.password, 6),
+          telephone: user.telephone ?? null,
+          actype: user.actype
+        })
+      }
+    })
+    
+    await mysql_connection.end()
   } catch (error) {
-    throw new Error(error)
+    throw error
   }
 }
 
-loadUsers()
+seed().then(() => {
+  console.log('🎉🎉 SUCESSO! Seu banco de dados foi criado com sucesso, agora você está livre para usar 😁😎')
+  console.log('Lembre-se das credenciais para login viu? 😉')
+  console.table(users)
+})

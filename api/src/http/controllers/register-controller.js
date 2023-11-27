4cf9fb1dc901debaa4faff7ee825d000f8626d14
Gustavo Sales <85498221+Gusales@users.noreply.github.com>
2023-11-27 13:13:50 -0300

    feat: generate random password at create new user

--- api/src/http/controllers/register-controller.js
+++ api/src/http/controllers/register-controller.js
@@ -2,22 +2,24 @@ import { ZodError, z } from "zod";
 import { Register } from "../../services/register.js";
 import { UserAlreadyExistsError } from "../../services/errors/user-already-exists-error.js";
 
+import { generatePassword } from '../../utils/generate-password.js'
+
 export async function registerController(request, response) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
-    password: z.string().min(6, 'Password must be at least 6 characters long'),
     rm: z.number(),
     telephone: z.string().optional().default(undefined),
     actype: z.enum(['PROF', 'ADMIN', 'COORD'])
   })
 
   try {
-    const { email, name, rm, actype, telephone, password } = registerBodySchema.parse(request.body)
-    console.log('bateu?')
+    const { email, name, rm, actype, telephone } = registerBodySchema.parse(request.body)
+    const password = generatePassword()
+
     const register = new Register()
     const { user: { id } } = await register.execute({ name, email, password, rm, telephone, actype })
-
+    
     return response.status(201).send({ id })
   } catch (error) {
     if (error instanceof UserAlreadyExistsError) {
